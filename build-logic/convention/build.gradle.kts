4711980a337c375fc9444183219621d4d1c8b075
Telma Popova <alexeypopov566@gmail.com>
2025-06-18 22:48:03 +0200

    TD-31: set up build-logic module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    `kotlin-dsl`
+}
+
+//group = "com.google.samples.modularization.buildlogic"
+group = "de.telma.todolist.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.tools.build.gradle.plugin)
+    compileOnly(libs.kotlin.gradle.plugin)
+}
+
+gradlePlugin {
+    /**
+     * Register convention plugins so they are available in the build scripts of the application
+     */
+    /*plugins {
+        register("sampleAndroidApplication") {
+            id = "sample.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("sampleAndroidLibrary") {
+            id = "sample.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("sampleAndroidTest") {
+            id = "sample.android.test"
+            implementationClass = "AndroidTestConventionPlugin"
+        }
+        register("sampleCompose") {
+            id = "sample.compose"
+            implementationClass = "ComposeConventionPlugin"
+        }
+        register("sampleDynamic") {
+            id = "sample.dynamic"
+            implementationClass = "DynamicFeatureConventionPlugin"
+        }
+    }*/
+}
